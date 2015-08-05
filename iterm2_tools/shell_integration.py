a67bfd8767842caaf4420bc47733a54792c61244
Aaron Meurer <asmeurer@gmail.com>
2015-08-04 21:40:34 -0500

    Add some documentation to the shell integration functions

--- iterm2_tools/shell_integration.py
+++ iterm2_tools/shell_integration.py
@@ -6,6 +6,34 @@ for documentation on the sequences,
 https://github.com/gnachman/iterm2-website/tree/master/source/misc for example
 implementations, and https://iterm2.com/shell_integration.html for a list of
 what this lets you do in iTerm2.
+
+Usage
+=====
+
+Say you have a basic REPL like
+
+input> run-command
+command output
+
+where "input> " is the prompt, "run-command" is the command typed by the user,
+and "command output" is the output of run-command. The basic REPL (in Python
+3), would be
+
+while True:
+    before_prompt()
+    print("input> ", end='')
+    after_prompt()
+    command = input()
+    before_output()
+    return_val = run_command(command)
+    after_output(return_val)
+
+(here return_val should be in the range 0-255).
+
+Note that it is recommended to use the functions (like before_prompt()) rather
+than the variables (like BEFORE_PROMPT) directly, as variables may break
+readline's character counting.
+
 """
 from __future__ import print_function, division, absolute_import
 
@@ -34,15 +62,29 @@ REMOTE_HOST = '\033]1337;RemoteHost={remote_host_username}@{remote_host_hostname
 CURRENT_DIR = '\033]1337;CurrentDir={current_dir}\a'
 
 def before_prompt():
+    """
+    Shell sequence to be run before the prompt.
+    """
     sys.stdout.write(BEFORE_PROMPT)
 
 def after_prompt():
+    """
+    Shell sequence to be run after the prompt.
+    """
     sys.stdout.write(AFTER_PROMPT)
 
 def before_output():
+    """
+    Shell sequence to be run before the command output.
+    """
     sys.stdout.write(BEFORE_OUTPUT)
 
 def after_output(command_status):
+    """
+    Shell sequence to be run after the command output.
+
+    The command_status should be in the range 0-255.
+    """
     if command_status not in range(256):
         raise ValueError("command_status must be an integer in the range 0-255")
     sys.stdout.write(AFTER_OUTPUT.format(command_status=command_status))
