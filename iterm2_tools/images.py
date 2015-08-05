4b85c19c30ac5f1c6678d83bef8cbf4472ed4cf1
Aaron Meurer <asmeurer@gmail.com>
2015-08-04 20:39:26 -0500

    Remove "iterm2" from function names

--- iterm2_tools/images.py
+++ iterm2_tools/images.py
@@ -3,9 +3,9 @@ import os
 import base64
 
 # See https://iterm2.com/images.html
-IMAGE_CODE= '\033]1337;File={file};inline={inline};size={size}:{base64_img}\a'
+IMAGE_CODE = '\033]1337;File={file};inline={inline};size={size}:{base64_img}\a'
 
-def iterm2_image_bytes(b, filename=None, inline=1):
+def image_bytes(b, filename=None, inline=1):
     data = {
         'file': base64.b64encode((filename or 'Unnamed file').encode('utf-8')).decode('ascii'),
         'inline': inline,
@@ -14,11 +14,11 @@ def iterm2_image_bytes(b, filename=None, inline=1):
         }
     return (IMAGE_CODE.format(**data))
 
-def iterm2_display_image_file(fn):
+def display_image_file(fn):
     """
     Display an image in the terminal.
 
     A newline is not printed.
     """
     with open(os.path.realpath(os.path.expanduser(fn)), 'rb') as f:
-        sys.stdout.write(iterm2_image_bytes(f.read(), filename=fn))
+        sys.stdout.write(image_bytes(f.read(), filename=fn))
--- iterm2_tools/tests/test_images.py
+++ iterm2_tools/tests/test_images.py
@@ -1,16 +1,16 @@
 import os
 
-from iterm2_tools import iterm2_image_bytes, iterm2_display_image_file
+from iterm2_tools import image_bytes, display_image_file
 
 # A one-pixel black gif
 one_pixel = b'GIF87a\x02\x00\x02\x00\x80\x00\x00\x00\x00\x00\xff\xff\xff,\x00\x00\x00\x00\x02\x00\x02\x00\x00\x02\x02\x84Q\x00;'
 
 def test_iterm2_image_bytes():
-    assert iterm2_image_bytes(one_pixel) == '\033]1337;File=VW5uYW1lZCBmaWxl;inline=1;size=35:R0lGODdhAgACAIAAAAAAAP///ywAAAAAAgACAAACAoRRADs=\a'
+    assert image_bytes(one_pixel) == '\033]1337;File=VW5uYW1lZCBmaWxl;inline=1;size=35:R0lGODdhAgACAIAAAAAAAP///ywAAAAAAgACAAACAoRRADs=\a'
 
 def test_iterm2_display_image_file():
     curdir = os.path.split(__file__)[0]
-    iterm2_display_image_file(os.path.join(curdir, 'aloha_cat.png'))
+    display_image_file(os.path.join(curdir, 'aloha_cat.png'))
     print()
 
 if __name__ == '__main__':
