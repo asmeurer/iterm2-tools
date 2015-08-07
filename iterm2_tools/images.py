b95d7501dff6df10eeecf2710dd73952c96b6c1a
Aaron Meurer <asmeurer@gmail.com>
2015-08-06 19:25:48 -0500

    Rename image_bytes to display_image_bytes

--- iterm2_tools/images.py
+++ iterm2_tools/images.py
@@ -7,7 +7,7 @@ import base64
 # See https://iterm2.com/images.html
 IMAGE_CODE = '\033]1337;File={file};inline={inline};size={size}:{base64_img}\a'
 
-def image_bytes(b, filename=None, inline=1):
+def display_image_bytes(b, filename=None, inline=1):
     """
     Display the image given by the bytes b in the terminal.
 
@@ -29,4 +29,7 @@ def display_image_file(fn):
     A newline is not printed.
     """
     with open(os.path.realpath(os.path.expanduser(fn)), 'rb') as f:
-        sys.stdout.write(image_bytes(f.read(), filename=fn))
+        sys.stdout.write(display_image_bytes(f.read(), filename=fn))
+
+# Backwards compatibility
+image_bytes = display_image_bytes
